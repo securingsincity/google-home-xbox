767709f7c0118dd91ea03fc6e4eb00fd38a16293
James Hrisho <james.hrisho@gmail.com>
2018-08-17 23:03:46 -0400

    add poweroff

--- index.ts
+++ index.ts
@@ -8,11 +8,14 @@ interface XBOXInstance {
 
 const xboxOn: XBOXConstructor = require("xbox-on");
 import * as express from 'express'
+import { exec } from 'child_process';
 
 
 const app = express()
 const deviceIp = process.env.XBOX_IP || '192.168.1.1'
 const deviceId = process.env.XBOX_DEVICE_ID || 'FOOBARBAZ'
+const username = process.env.XBOX_USERNAME || 'FOOBARBAZ'
+const password = process.env.XBOX_PASSWORD || 'FOOBARBAZ'
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -23,6 +26,11 @@ app.post('/power', function(req, res) {
   xbox.powerOn()
   res.json({status: 'success'})
 });
+
+app.delete('/power', function (req, res) {
+  exec(`xbox-authenticate --email ${username} --password ${password}`);
+  exec(`xbox-poweroff --liveid ${deviceId}`);
+})
 
 app.listen(3000, function () {
   console.log('Xbox Power App listening on port 3000!')
